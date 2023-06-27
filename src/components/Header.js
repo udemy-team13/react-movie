ca5bc65f0ece5efb924798f2f6c8069230bad254
gonasooc <doupler@gmail.com>
2023-06-27 15:37:45 +0900

    header 변경

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,36 @@
-import React from 'react';
 import { Link } from 'react-router-dom';
+import Logo from 'assets/images/logo.png'
 
 const Header = () => {
   return (
     <header className='header'>
       <div className="header-inner">
-        <nav className="header-nav">
-          <ul className='header-nav-grp'>
-            <li className='header-nav-list'>
-              <Link to="/">MovieList</Link>
-            </li>
-          </ul>
-        </nav>
+        <div className="flex-wrap">
+          <div className="left-wrap">
+            <Link to="/">
+              <div className="logo-img-grp">
+                <img src={Logo} alt="logo-img" className='img' />
+              </div>
+            </Link>
+            <nav className="header-nav">
+              <ul className='header-nav-grp'>
+                <li className='header-nav-list'>
+                  <Link to="/">reactMovie()</Link>
+                </li>
+              </ul>
+            </nav>
+          </div>
+          <div className="right-wrap">
+            {/* 고도화 시 작업해 볼 예정..? */}
+            {/* <div className="search-grp">
+              <input type="text" onChange={(e) => {
+                setKeyword(e.target.value);
+                console.log(keyword);
+              }} />
+              <button type="button">검색</button>
+            </div> */}
+          </div>
+        </div>
       </div>
     </header>
   )
