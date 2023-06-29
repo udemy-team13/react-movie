b33f091eb833a75854b677bd8c21b50cd9257153
gonasooc <doupler@gmail.com>
2023-06-29 22:46:33 +0900

    무한스크롤 작업중

---
+++ src/components/SectionObserver.js
@@ -0,0 +1,3 @@
+export function SectionObserver(props) {
+  return <div ref={props.target} style={{height: '100px', backgroundColor: 'red'}}></div>
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,20 +1,48 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Loader from "components/Loader";
 import MovieList from "pages/MovieList";
+import { SectionObserver } from "components/SectionObserver";
 
 export default function Home() {
+  const target = useRef(null);
+
   const [loading, setLoading] = useState(true);
   const [movies, setMovies] = useState([]);
 
+  const [page, setPage] = useState(1);
+
   useEffect(() => {
-    (async () => {
-      const { data } = await (
-        await fetch("https://yts.mx/api/v2/list_movies.json")
-      ).json();
-      setLoading(() => false);
-      setMovies(data.movies);
-    })();
+    observer.observe(target.current);
+    fetchData(true);
   }, []);
 
-  return <>{loading ? <Loader /> : <MovieList movies={movies} />}</>;
+  function fetchData(reset = false) {
+    (async () => {
+      const { data } = await (
+        await fetch(`https://yts.mx/api/v2/list_movies.json?page=${page}`)
+      ).json();
+      setLoading(() => false);
+      reset ? setMovies(data.movies) : setMovies(movies.concat(data.movies));
+      // reset ? setPage(1) : setPage(page + 1);
+    })();
+  }
+
+  const options = {
+    threshold: 1.0,
+  }
+
+  const callback = () => {
+    target.current.innerText += "관측됨";
+    fetchData();
+    // setPage(page + 1);
+  };
+
+  const observer = new IntersectionObserver(callback, options);
+
+  return (
+    <>
+      {loading ? <Loader /> : <MovieList movies={movies} />}
+      <SectionObserver target={target} />
+    </>
+  )
 }
