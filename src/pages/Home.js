337d8464e56ea68895b135cfe7ff1c891b97a378
gonasooc <doupler@gmail.com>
2023-06-29 19:19:04 +0900

    #27 fix

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,7 +11,6 @@ export default function Home() {
       const { data } = await (
         await fetch("https://yts.mx/api/v2/list_movies.json")
       ).json();
-      // setLoading((prev) => !prev); // 이거 왜 !prev인지 이해를 못해서 일단 잠시 홀딩..
       setLoading(() => false);
       setMovies(data.movies);
     })();
